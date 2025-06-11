6a4b1b3c6675c4de9d3a54cbf099d9a73db200fe
Edison <77561470+edison16a@users.noreply.github.com>
2025-06-11 16:34:03 -0700

    Update StartupCard.tsx

--- components/ui/StartupCard.tsx
+++ components/ui/StartupCard.tsx
@@ -36,7 +36,13 @@ const StartupCard = ({post}: {post: StartupTypeCard}) => {
                 </Link>
             </div>
             <Link href={`/user/${author?._id}`}>
-                <Image src="https://images.unsplash.com/photo-1749310112178-d0e62994b0e0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="placeholder" width={48} height={48} className="rounded-full"></Image>
+                <Image
+            src={author?.image!}
+            alt={author?.name!}
+            width={48}
+            height={48}
+            className="rounded-full"
+                  />
             </Link>
         </div>
 
